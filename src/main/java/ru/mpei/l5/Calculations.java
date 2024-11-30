be367525bc8fd9b1dddf62226205ec9101bc0f04
ltKy3mu4-home <rogozinnikovei@yandex.ru>
2024-11-30 12:42:25 +0300

    added Calculations class and methods sum and factorials

---
+++ src/main/java/ru/mpei/l5/Calculations.java
@@ -0,0 +1,19 @@
+package ru.mpei.l5;
+
+public class Calculations {
+
+    public static double sum(double s1, double s2){
+        return s1 + s2;
+    }
+
+    public static long factorial(int n){
+        long s = n;
+        long currentN = n;
+        for(int i =n-1; i > 0; i--){
+            currentN = currentN - 1;
+            s = s * currentN;
+        }
+        return s;
+    }
+
+}
